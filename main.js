fe9152e7be0ae242e0fc81113bd457a5d32e4c7e
Kisant <160364191+Kisant1126@users.noreply.github.com>
2024-05-21 20:23:09 -0500

    add *

--- main.js
+++ main.js
@@ -1,98 +1,137 @@
-//rock paper and scisors
-function getComputerChoice(){
-    let RandomChoice=Math.round(Math.random()*3);
-    let Choice;
-    if (RandomChoice==1 || RandomChoice==0){
-        return Choice="paper";
-    }else if(RandomChoice==2){
-       return Choice="scissors";
-    }else if(RandomChoice ==3){
-        return Choice="rock";
+//rock paper and scissors
+let selection = [
+  "images/piedra.png",
+  "images/hoja-de-papel.png",
+  "images/tijeras.png",
+];
+
+let choice;
+let computer = document.querySelector(".computer");
+let boton;
+let randomChoice;
+let recurso=document.querySelector(".recurso")
+let playnew=document.querySelector("#continue")
+let container=document.querySelector(".container")
+let invisible=document.querySelector(".invisible")
+let important=document.querySelector(".important")
+let start=document.querySelector("#start")
+let container2=document.querySelector("#container")
+let invisible2=document.querySelector(".invisible2")
+let playerwins=0
+let computerwins=0
+let champion=document.querySelector(".champion")
+start.addEventListener("click",()=>{
+  recurso.style.display="flex"
+  start.style.display="none"
+  container2.style.display="none"
+  playerPuntuation=0
+  computerPuntuation=0
+})
+function getComputerSelection() {
+  randomChoice = Math.round(Math.random() * 2);
+  choice = selection[randomChoice];
+  computer.setAttribute("src", choice);
+}
+let buttons = document.querySelectorAll("button");
+let player = document.querySelector(".player");
+//eleccion en pantalla del jugador
+buttons.forEach((btns) => {
+  btns.addEventListener("click", () => {
+    if (btns.id === "piedra") {
+      player.setAttribute("src", "images/piedra.png");
+      boton = 0;
+      computer.style.display="none"
+      invisible.style.display="block"
+      important.style.display="none"
+      getComputerSelection()
+    } else if (btns.id === "papel") {
+      player.setAttribute("src", "images/hoja-de-papel.png");
+      boton = 1;
+      computer.style.display="none"
+      getComputerSelection()
+      invisible.style.display="block"
+      important.style.display="none"  
+    } else if (btns.id === "tijera") {
+      player.setAttribute("src", "images/tijeras.png");
+      boton = 2;
+      computer.style.display="none"
+      getComputerSelection()
+      invisible.style.display="block"
+      important.style.display="none"
+     
+    } else if (btns.id === "playround") {
+        win_lose()
+        computer.style.display=""
     }
+  });
+});
+//como saber si se gana o se pierde
+let playerPuntuation=0
+let computerPuntuation=0
+function verification(){
+  invisible2.style.display="block"
+  invisible2.textContent=`puntuation: player: ${playerPuntuation}- computer:${computerPuntuation} `
+  if(playerPuntuation>4){
+    important.textContent="player Wins"
+    recurso.style.display="none"
+    container2.style.display="flex"
+    start.style.display="block"
+    playnew.style.display="none"
+    invisible2.style.display="none"
+    invisible.style.display="none"
+    playerwins++
+    champion.textContent=`Wins player:${playerwins} - Wins computer:${computerwins}`
+  }else if(computerPuntuation>4){
+    important.textContent="computer wins"
+    recurso.style.display="none"
+    container2.style.display="flex"
+    start.style.display="block"
+    playnew.style.display="none"
+    invisible2.style.display="none"
+    invisible.style.display="none"
+    computerwins++
+    champion.textContent=`Wins player:${playerwins} - Wins computer:${computerwins}`
+  }
 }
-
-let PlayerSelection;
-function Verification2()
-{if(PlayerSelection=="scissors"){
-    PlayerSelection=1
-}else if(PlayerSelection=="paper"){
-    PlayerSelection=2
-}else if(PlayerSelection=="rock"){
-    PlayerSelection=3
-}
-}
-
-function PlayRound(PlayerSelection,ComputerSelection){
-    let result;
-    if(ComputerSelection=="paper" && PlayerSelection==3){
-        return console.log("You Lose! Paper beats Rock"),
-        result=-1;
-    }else if(ComputerSelection=="paper" && PlayerSelection==1){
-        return console.log("You Win! Scissors beats paper"),
-        result=1;
-    }else if(ComputerSelection=="rock" && PlayerSelection==1){
-        return console.log("You Lose! Rock beats Scissors"),
-        result=-1;
-    }else if(ComputerSelection=="rock" && PlayerSelection==2){
-        return console.log("You Win ! Paper beats Rock"),
-        result=1;
-    }else if(ComputerSelection=="scissors" && PlayerSelection==2){
-        return console.log("You Lose! Scissors beats Paper"),
-        result=-1;
-    }else if(ComputerSelection=="scissors" && PlayerSelection==3){
-        return console.log("You Win! Rock beats Scissors"),
-        result=1;
-    }else if(ComputerSelection=="paper" && PlayerSelection==2){
-        return console.log("Its a tie Paper and Paper"),
-        result=0;
-    }else if(ComputerSelection=="rock" && PlayerSelection==3){
-        return console.log("Its a tie Rock and Rock"),
-        result=0;
-    }else if(ComputerSelection=="scissors" && PlayerSelection==1){
-        return console.log("Its a tie Scissors and Scissors"),
-        result=0;
-    }else{
-        return console.log("you didn't write what was indicated"),
-        result=-1;
-    }
-}
-
-let PlayerPuntuation=0;
-let ComputerPuntuation=0;
-function Puntuation(PlayRound){
-    if(PlayRound==1){
-        return PlayerPuntuation=++PlayerPuntuation;
-    }else if(PlayRound==-1){
-        return ComputerPuntuation=++ComputerPuntuation;
-    }else if(PlayRound==0){
-        return ComputerPuntuation=ComputerPuntuation,
-        PlayerPuntuation=PlayerPuntuation;
-    }   
-}
-
-function result(){
-    if(ComputerPuntuation>PlayerPuntuation){
-        console.log("Computer wins")
-    }else if(ComputerPuntuation<PlayerPuntuation){
-        console.log("Player wins")
-    }else{
-        console.log("Its a tie")
-    }
-}
-
-function PlayGame(){
-    PlayerSelection=prompt("give me your choice between rock paper and scissors");
-    let Verification =PlayerSelection.toLowerCase();
-      PlayerSelection=Verification;
-      Verification2();
-    let ComputerSelection =getComputerChoice();
-    Puntuation(PlayRound(PlayerSelection,ComputerSelection));
-    console.log (`computer:${ComputerPuntuation}-player:${PlayerPuntuation}`)
-}
-
-PlayGame()
-PlayGame()
-PlayGame()
-PlayGame()
-PlayGame()
-result()
+function win_lose() {
+  if (
+    (boton == 0 && randomChoice == 2) ||
+    (boton == 1 && randomChoice == 0) ||
+    (boton == 2 && randomChoice == 1)
+  ) {
+    console.log("you win"),
+    recurso.style.display="none"
+    playnew.style.display="block"
+    container.style.cssText="align-items:center;justify-content:center;";
+    playerPuntuation++ 
+  } else if (
+    (boton == 0 && randomChoice == 1) ||
+    (boton == 1 && randomChoice == 2) ||
+    (boton == 2 && randomChoice == 0)
+  ) {
+    console.log("you lose"),
+    recurso.style.display="none"
+    playnew.style.display="block"
+    container.style.cssText="align-items:center;justify-content:center;"
+    computerPuntuation++
+  }else if(    
+  (boton == 0 && randomChoice == 0) ||
+  (boton == 1 && randomChoice == 1) ||
+  (boton == 2 && randomChoice == 2))
+    {
+    console.log("is a tie")
+    recurso.style.display="none"
+    playnew.style.display="block"
+    container.style.cssText="align-items:center;justify-content:center;"
+    computerPuntuation
+    playerPuntuation
+  }}
+  //para jugar una ronda nueva
+  playnew.addEventListener("click",()=>{
+    playnew.style.display="none"
+    recurso.style.display="flex"
+    invisible.style.display="none"
+    important.style.display="block"
+    boton=4
+    verification()
+  })
